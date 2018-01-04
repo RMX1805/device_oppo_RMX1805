a74a5d2cf260f78b92289b2bce79387a826d4a36
dianlujitao <dianlujitao@lineageos.org>
2018-01-04 09:48:59 +0000

    RMX1805: Build dummy android.hidl.[base,manager]@1.0
    Change-Id: Idae6c4fedd687e8fe1ce24f680f5f386862c12bb

--- device.mk
+++ device.mk
@@ -14,5 +14,10 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.manager@1.0 \
+    android.hidl.base@1.0
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oppo/RMX1805/RMX1805-vendor.mk)
