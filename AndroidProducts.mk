680ed9a9aa24b58b2ca5cb4b0463eda185a1b3e9
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-08-21 11:23:00 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1805-user \
-    lineage_RMX1805-userdebug \
-    lineage_RMX1805-eng
+    carbon_RMX1805-user \
+    carbon_RMX1805-userdebug \
+    carbon_RMX1805-eng
