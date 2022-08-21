ee7f0b75143f576f26a161c6057526b43b181421
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-08-21 12:23:05 +0530

    Update carbon_RMX1805.mk

--- carbon_RMX1805.mk
+++ carbon_RMX1805.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
 
 # Inherit from RMX1805 device
 $(call inherit-product, device/oppo/RMX1805/device.mk)
