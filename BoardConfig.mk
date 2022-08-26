8916aa6925d1fdcd5575ddb06833c05a759ee7ff
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-08-26 08:05:31 +0530

    Update BoardConfig.mk
    added # Init
    TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_msm8953
    TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,10 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_msm8953
+TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7
